cc40a0774de02d8e5e3871fb381b5e3fba6de9c2
reddy1238 <76726166+reddy1238@users.noreply.github.com>
2020-12-29 15:53:34 +0530

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -2,34 +2,41 @@ pipeline {
         agent any
                 stages {
                        stage('One') {
-                               steps { echo 'Hi, this is sailesh'
+                               steps { 
+                                       echo 'Hi, this is sailesh'
                                 }
                        }
                        stage('Two') {
-                               steps { input('Do you want to proceed?'}
+                               steps { 
+                                       input('Do you want to proceed?'}
                                 }
                        }
                        stage('Three') {
                                when {
-                                       not { branch 'master'
+                                       not { 
+                                               branch 'master'
                                        }
                                  }
-                                 steps { echo "Hello"
+                                 steps {
+                                         echo "Hello"
                                  }
                        }
                        stage('Four') {
                                        parallel {
                                              stage('Unit Test') {
-                                                                steps { echo "Running the unit test..."
+                                                                steps { 
+                                                                        echo "Running the unit test..."
                                                                 }
                                              }
                                              stage('Integration test') {
                                                                 agent {
-                                                                      docker { reuseNode false
+                                                                      docker { 
+                                                                              reuseNode false
                                                                                image 'ubuntu'
                                                                        }
                                                                  }
-                                                                 steps { echo 'Running the integration test...'
+                                                                 steps { 
+                                                                         echo 'Running the integration test...'
                                                                  }
                                }
                           }
